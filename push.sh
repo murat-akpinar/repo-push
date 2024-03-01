883d495f83e28b159e32963482830f0cb5ec79d7
murat-akpinar <akpinarmurat@protonmail.com>
2024-03-01 11:24:33 +0300

    push

---
+++ push.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Kullanıcıdan alınan tar dosyasının adı
+TAR_FILE=$1
+
+# Repo URL'si
+REPO_URL="saganak-nexus-tls.scope.kkuhtm.aynet.com.tr"
+today=$(date +%Y%m%d)
+
+# Eğer TAR_FILE argümanı verilmemişse, hata mesajı göster ve çık
+if [ -z "$TAR_FILE" ]; then
+    echo "Lütfen bir tar dosyası adı girin."
+    echo "Kullanım: $0 <dosya.tar>"
+    exit 1
+fi
+
+# Docker imajını yükle ve imaj adını al
+output=$(docker load -i "$TAR_FILE")
+loaded_image=$(echo $output | grep -oP '(?<=Loaded image: )[^ ]+')
+
+if [ -z "$loaded_image" ]; then
+    echo "Imaj yüklenemedi veya imaj adı bulunamadı."
+    exit 1
+fi
+
+# İmaj adını ve tag'ını ayır (eğer varsa)
+image_name=$(echo $loaded_image | cut -d ':' -f 1)
+image_tag=$(echo $loaded_image | cut -d ':' -f 2)
+
+# Eğer yüklenen imajda tag yoksa, 'latest' olarak varsay
+if [ -z "$image_tag" ]; then
+    image_tag="latest"
+fi
+
+# Yeni tag ile docker tag ve docker push
+new_tag="${REPO_URL}/${image_name}:${today}"
+docker tag "${image_name}:${image_tag}" "${new_tag}"
+docker push "${new_tag}"
+
